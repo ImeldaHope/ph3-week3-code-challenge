a5ee81f0ae2f889df35e984fbd088ce17075d427
Imelda Hope <imeldalarusso@gmail.com>
2024-09-17 23:49:46 +0300

    creates aggregate and relationship methods

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData, DateTime, func
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -16,12 +16,39 @@ class Band(Base):
     name= Column(String())
     hometown = Column(String())
     
-    concerts = relationship('Concert', backref=backref('band'))    
+    _concerts = relationship('Concert', backref=backref('band'))    
     
     def __repr__(self):
         return f'Band(id={self.id}, ' + \
             f'name={self.name}, ' + \
             f'hometown={self.hometown})'
+    
+    def concerts(self):
+        return self._concerts
+
+    def venues(self):
+        return [concert.venue for concert in self._concerts]
+    
+    def play_in_venue(self, venue, date):        
+        concert = Concert(band_id=self.id, venue_id=venue.id, date=date)        
+        return concert
+
+    def all_introductions(self):
+        """Returns an array of strings representing all the introductions for this band."""
+        return [concert.introduction() for concert in self.concerts]
+
+    @classmethod
+    def most_performances(cls):       
+        band_counts = (
+            cls.query
+            .outerjoin(Concert)
+            .group_by(cls.id)
+            .with_entities(cls, func.count(Concert.id).label('performance_count'))
+            .all()
+        )       
+        
+        most_performances_band = max(band_counts, key=lambda x: x.performance_count)
+        return most_performances_band[0]
     
 class Venue(Base):
     __tablename__ = 'venues'
@@ -30,12 +57,34 @@ class Venue(Base):
     title = Column(String())
     city = Column(String())
 
-    concerts = relationship('Concert', backref=backref('venue'))
+    _concerts = relationship('Concert', backref=backref('venue'))
        
     def __repr__(self):
         return f'Venue(id={self.id}, ' + \
             f'title={self.title}, ' + \
             f'city={self.city})'
+    
+    def concerts(self):
+        return self._concerts
+
+    def bands(self):
+        return [concert.band for concert in self._concerts]
+    
+    def concert_on(self, date):        
+        return Concert.query.filter_by(venue_id=self.id, date=date).first()
+
+    def most_frequent_band(self):
+        band_counts = (
+        Band.query
+        .join(Concert)
+        .filter(Concert.venue_id == self.id)
+        .group_by(Band.id)
+        .with_entities(Band, func.count(Concert.id).label('performance_count'))
+        .all()
+        )
+
+        most_frequent_band = max(band_counts, key=lambda x: x.performance_count)
+        return most_frequent_band[0]
     
 class Concert(Base):
     __tablename__ = 'concerts'
@@ -50,3 +99,15 @@ class Concert(Base):
                f'date={self.date}, ' + \
                f'band_id={self.band_id}, ' + \
                f'venue_id={self.venue_id})'
+    
+    def band(self):
+        return self.band
+
+    def venue(self):
+        return self.venue
+    
+    def hometown_show(self):        
+        return self.venue.city == self.band.hometown
+
+    def introduction(self):        
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
